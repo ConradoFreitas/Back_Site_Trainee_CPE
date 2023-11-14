eda396ced6ad6748d8a1d41c271655384359e1a6
Gustavo Melo <gustavomtmelo@gmail.com>
2023-11-14 13:20:12 -0300

    update/ configs do express

--- routes.js
+++ routes.js
@@ -1,10 +1,19 @@
 const { Router } = require("express");
 const UsuarioController = require("./Controllers/UsuarioController");
+const SessoesController = require ("./Controllers/SessoesCrotroller");
 
 const rotas = Router();
 
+//Usuários
 rotas.post("/usuarios", UsuarioController.create);
 rotas.get("/usuarios", UsuarioController.read);
 rotas.delete("/usuarios/:id", UsuarioController.delete);
+rotas.put("/usuario/:id", UsuarioController.update);
+
+//Sessoes
+rotas.post("/sessoes", SessoesController.create);
+rotas.get("/sessoes", SessoesController.read);
+rotas.delete("/sessoes/:id", SessoesController.delete);
+
 
 module.exports = rotas;
